3dad7b4d616368bc0fcf0ad906804a29d3d9fd0a
Kevin Zhang <kzhang@ivani.com>
2019-03-04 12:31:04 -0500

    created new dataset for variable length inputs, will try to make it work later

--- medium_char_GRU.py
+++ medium_char_GRU.py
@@ -95,7 +95,8 @@ if __name__=="__main__":
     print(gru)
 
     trainer = Trainer()
-    dummy_data = "chicken chimera chilled chickee present precise precede presort presold prewash " * 100
+    # dummy_data = "chicken chimera chilled chickee present precise precede presort presold prewash " * 100
+    dummy_data = "autonomy automan automatic autograph automobile autotransformer autobiography autocracy autoimmune autotrophic chickadee chickenshit chickens chickaree chicks bigmouth biggie bigotry biggity biggest"
     trainer.set_data(dummy_data)
 
     optimizer = optim.Adam(gru.parameters(), lr=.0005)
